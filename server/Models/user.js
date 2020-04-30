99809094e6e58438df3d7f8b734767e6d169f416
Brad Donakowski <braddonako@gmail.com>
2020-04-30 11:58:15 -0600

    created a pre function for the user that will hash the password

--- server/Models/user.js
+++ server/Models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SALT_I = 10; 
 
 const userSchema = mongoose.Schema({
     email: {
@@ -31,6 +34,29 @@ const userSchema = mongoose.Schema({
         type: String
     }
 })
+
+// going to hash the password first when the data is sent
+userSchema.pre('save', function(next){
+    let user = this;
+
+    //now lets encrypt the password
+    if (user.isModified('password')){
+        bcrypt.genSalt(SALT_I, function(err, salt){
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if (err) return next(err)
+                user.password = hash;
+                next()
+            })
+        })
+    } else {
+        next();
+    }
+})
+
+// i am now going to use bcrypt to 
+
 
 const User = mongoose.model('User', userSchema);
 module.exports = { User }
