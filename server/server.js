82c3259eb4450a247f188db3eacb8946659e3fb9
Brad Donakowski <braddonako@gmail.com>
2020-06-11 12:15:08 -0600

    I can now find jobs by their id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+
 const app = express()
 
 
@@ -32,6 +33,66 @@ const { Job } = require('./Models/job')
 const { auth } = require('./Middleware/auth')
 
 
+//===============================
+//         Job Routes
+// ==============================
+
+// get job by id
+app.get('/api/jobs', auth, (req, res) => {
+    // const allJobs = req.user.jobs;
+    // res.json({allJobs});
+    let type = req.query.type;
+    let items = req.query.id;
+
+    if(type === "array"){
+        let ids = req.query.id.split(',');
+        items = [];
+        items = ids.map(item => {
+            return mongoose.Types.ObjectId(item)
+        })
+    }
+
+    Job
+    .find({'_id': {$in:items}}).
+    exec((err, docs)=> {
+        return res.status(200).send(docs)
+    });
+
+
+    
+})
+
+// add a new job to your board
+app.post('/api/jobs/addJob', auth, (req, res) => {
+
+    const job = new Job(req.body)
+    // console.log('is it even making it here')
+    // console.log(req.user.jobs, '<-- this is during the job')
+    console.log(job, '<-- this is the job');
+    // job.save()
+    const currentUser = req.user;
+    User.findById({currentUser})
+    // console.log(currentUser, 'this is the user')
+    if(currentUser){
+        // console.log(currentUser, 'user id in if')
+        console.log(job, 'here is the current job, line 78');
+        currentUser.jobs.push(job);
+        
+        currentUser.save();
+        console.log(currentUser, 'here is the current user after the job saves')
+        res.json({message: 'Job has been posted!!!!'})
+    }
+
+})
+
+// delete job from your board
+app.delete('/api/jobs/delete/:id', function (){
+    
+})
+
+app.get('/', (req, res)=>{
+     res.send('Hey look here is my server')
+})
 
 //===============================
 //         User Routes
@@ -51,11 +112,14 @@ app.get('/api/users/auth', auth, (req, res) => {
 
 
 //Register an account
-app.post('/api/users/register', (req,res)=>{
+app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
 
     user.save((err, doc) => {
-        if(err) return res.json({success: false, err})
+        if (err) return res.json({
+            success: false,
+            err
+        })
         res.status(200).json({
             success: true,
             userdata: doc
@@ -64,96 +128,48 @@ app.post('/api/users/register', (req,res)=>{
 })
 
 //login
-app.get('/api/users/login', (req,res)=> {
+app.get('/api/users/login', (req, res) => {
     // going to search for the email first
-    User.findOne({'email': req.body.email}, (err,user)=> {
-        if(!user) return res.json({loginSuccess: false, message: 'your email is incorrect'})
+    User.findOne({
+        'email': req.body.email
+    }, (err, user) => {
+        if (!user) return res.json({
+            loginSuccess: false,
+            message: 'your email is incorrect'
+        })
 
         user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch) return res.json({loginSuccess: false, message: 'your password is incorrect'})
+            if (!isMatch) return res.json({
+                loginSuccess: false,
+                message: 'your password is incorrect'
+            })
         })
 
         user.generateToken((err, user) => {
             if (err) return res.status(400).send(err)
-            res.cookie('x_auth', user.token).status(200).json({loginSuccess: true})
+            res.cookie('x_auth', user.token).status(200).json({
+                loginSuccess: true
+            })
         })
     })
 })
 
 
 //logout
-app.get('/api/users/logout', auth, (req,res)=>{
+app.get('/api/users/logout', auth, (req, res) => {
     User.findOneAndUpdate(
-    {_id: req.user._id}, 
-    {token: ''},
-    (err, doc) => {
-        if (err) return res.json({success: false, err})
-        return res.status(200).send({
-            success: true
-        })
-    }
+        { _id: req.user._id }, 
+        { token: ''},
+        (err, doc) => {
+            if (err) return res.json({
+                success: false,
+                err
+            })
+            return res.status(200).send({
+                success: true
+            })
+        }
     )
-})
-
-//===============================
-//         Job Routes
-// ==============================
-
-app.get('/api/jobs', auth, async (req, res) => {
-    const user = await User
-        .findById(req.params._id)
-        .populate('jobs');
-    res.send(user)
-
-})
-
-// add a new job to your board
-app.post('/api/jobs/addJob', auth, async (req, res) => {
-
-    const job = new Job(req.body)
-    // console.log('is it even making it here')
-    // console.log(req.user.jobs, '<-- this is during the job')
-    job.save()
-    const currentUser = req.user;
-    User.findById(currentUser)
-    console.log(currentUser, 'this is the user')
-    if(currentUser){
-        console.log(currentUser, 'user id in if')
-        currentUser.jobs.push(job);
-        currentUser.save();
-        res.json({message: 'Job has closed!!!!'})
-    }
-    // .then((result) => {
-
-
-    //     User.findOne({ user: job.id}, (err, user)=>{
-    //         console.log(user, 'this is the current user')
-    //         // const user = req.user.id;
-    //         console.log(req.user.id, '<-- this is the req.user.id')
-
-    //         if(user){
-    //             // console.log(user, "<-- this is the user")
-                
-    //             user.jobs.push(job);
-    //             user.save();
-    //             res.json({message: 'Job has been added!!'})
-    //         }
-    //     })
-    //     .catch((error) => {
-    //         result.status(500).json({ error });
-    //     })
-
-    // })
-
-})
-
-// delete job from your board
-app.delete('/api/jobs/delete/:id', function (){
-    
-})
-
-app.get('/', (req, res)=>{
-     res.send('Hey look here is my server')
 })
 
 
