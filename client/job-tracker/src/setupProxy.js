8dd84e8c571707ae17db7bf3cf15ea410f80cd46
Brad Donakowski <braddonako@gmail.com>
2020-06-12 16:29:19 -0600

    I am rattled

--- client/job-tracker/src/setupProxy.js
+++ client/job-tracker/src/setupProxy.js
@@ -1,10 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+// https://create-react-app.dev/docs/proxying-api-requests-in-development/#configuring-the-proxy-manually
+// link to proxying in react -- tried to proxy through package.json, and this was the solution
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/api/users', // replace with your endpoint
-        { target: 'http://localhost:3002',
+  app.use(
+      '/api/',
+      createProxyMiddleware({
+          target: 'http://localhost:3002',
           changeOrigin: true
-    } // replace with your target
-    ));
-}
+      })
+  )
+};
