d9355d51f22be70f9d5c819de169604d05654369
Brad Donakowski <braddonako@gmail.com>
2020-06-11 20:13:24 -0600

    Header is now added to the page

--- client/job-tracker/src/Components/hoc/layout.js
+++ client/job-tracker/src/Components/hoc/layout.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../Header_Footer/header'
 
 class Layout extends Component {
     render() {
         return (
             <div>
-            HEADER
+            <Header/>
             <div className='page_container'>
                 {this.props.children}
             </div>
--- client/job-tracker/src/routes.js
+++ client/job-tracker/src/routes.js
@@ -4,6 +4,7 @@ import {Switch, Route } from 'react-router-dom';
 import Home from './Components/Home/home';
 import Layout from './Components/hoc/layout';
 
+
 const Routes = () => {
   return(
     <Layout>
