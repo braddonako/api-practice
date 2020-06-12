865b0e7aed8a8cb76323dded9537f58f7a552c35
Brad Donakowski <braddonako@gmail.com>
2020-06-12 15:14:21 -0600

    new server start

--- client/job-tracker/src/setupProxy.js
+++ client/job-tracker/src/setupProxy.js
@@ -1,9 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/api/', // replace with your endpoint
-        { target: 'http://localhost:3002' } // replace with your target
+    app.use(createProxyMiddleware('/api/users', // replace with your endpoint
+        { target: 'http://localhost:3002',
+          changeOrigin: true
+    } // replace with your target
     ));
 }
